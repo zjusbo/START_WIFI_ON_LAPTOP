4b5f68e710163c7feb73006a8ddfcf9eb9839f5a
Sunny Song <sbo@zju.edu.cn>
2014-07-25 15:19:52 +0800

    Update startWift.bat

--- startWift.bat
+++ startWift.bat
@@ -5,7 +5,9 @@
 echo off
 echo.Starting hostednetwork
 echo.Id: MyWifi
-netsh wlan set hostednetwork mode=allow ssid=MyWifi key=123456 ::你可以在这里修改Wifi名称或Wifi密码
+netsh wlan set hostednetwork mode=allow ssid=MyWifi key=12345678
+::你可以在上一行修改Wifi名称或Wifi密码
 netsh wlan start hostednetwork
 echo.Start success.
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v wkdxz /t reg_sz /d %0 /f ::每次开机都自动启动，若不想每次开机都启动，请删掉此行
+::reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v wkdxz /t reg_sz /d %0 /f 
+::若想每次开机自动开启Wifi，请将上面一行开头的两个冒号"::"删去，保存运行即可
