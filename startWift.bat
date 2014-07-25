ae6589d1f2f7cbf33c75af3161a8e8022c20d764
Sunny Song <sbo@zju.edu.cn>
2014-07-25 15:28:45 +0800

    Update startWift.bat

--- startWift.bat
+++ startWift.bat
@@ -1,13 +1,13 @@
-::start wifi on your lapto automatically.
-::Win 7 version 
-::author: Song Bo, Eagle, Zhejiang University
-::email:sbo@zju.edu.cn
+rem start wifi on your lapto automatically.
+rem Win 7 version 
+rem author: Song Bo, Eagle, Zhejiang University
+rem email:sbo@zju.edu.cn
 echo off
 echo.Starting hostednetwork
 echo.Id: MyWifi
 netsh wlan set hostednetwork mode=allow ssid=MyWifi key=12345678
-::你可以在上一行修改Wifi名称或Wifi密码
+rem 你可以在上一行修改Wifi名称或Wifi密码
 netsh wlan start hostednetwork
 echo.Start success.
-::reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v wkdxz /t reg_sz /d %0 /f 
-::若想每次开机自动开启Wifi，请将上面一行开头的两个冒号"::"删去，保存运行即可
+rem reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v wkdxz /t reg_sz /d %0 /f 
+rem 若想每次开机自动开启Wifi，请将上面一行开头的rem删去，保存运行即可
